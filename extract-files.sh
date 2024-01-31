35fc8f7bd5473ca1c3392678ba3e651c6d468791
BladeRunner-A2C <john.smith@unused.email>
2024-01-31 01:52:10 -0500

    DRG: Use sed to patch fingerprint libraries
    * Some weird garbage happens on this lib and breaks _some_ features.
    
    Change-Id: I411be78c696f88b23a7ef603efa5b33223f25c7d

--- extract-files.sh
+++ extract-files.sh
@@ -12,11 +12,9 @@
 function blob_fixup() {
     case "${1}" in
         # Fix missing symbol _ZN7android8hardware7details17gBnConstructorMapE
-        vendor/lib64/libvendor.goodix.hardware.fingerprintextension@1.0.so)
-            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-            ;;
-        vendor/lib64/hw/fingerprint.gxfp3258_drg.so)
-            "${PATCHELF}" --add-needed "libhidlbase-v32.so" "${2}"
+        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so | vendor/lib64/libvendor.goodix.hardware.fingerprintextension@1.0.so)
+            "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
             ;;
     esac
 }
