7a7cd3caa22b11827756fd5b661d1b225e0672c4
Cosmin Tanislav <demonsingur@gmail.com>
2025-05-10 09:35:03 +0700

    DRG: Do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device.mk
+++ device.mk
@@ -38,11 +38,6 @@ TARGET_SCREEN_WIDTH := 1080
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    libhidlbase-v32.vendor \
-    liblzma.vendor
-
 # FM
 PRODUCT_PACKAGES += \
     FM2
